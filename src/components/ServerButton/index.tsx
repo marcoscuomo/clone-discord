966045267f91f9d23fec172827ddfd18aa193c53
Marcos Souza <bytecore@gmail.com>
2021-10-16 21:52:20 -0300

    Creating global styles

---
+++ src/components/ServerButton/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Button } from './styles';
+
+export interface IButtonProps {
+  selected?: boolean;
+  isHome?: boolean;
+  hasNotifications?: boolean;
+  mentions?: number;
+}
+
+const ServerButton: React.FC<IButtonProps> = ({
+  selected,
+  isHome,
+  hasNotifications,
+  mentions
+}) => {
+  return (
+    <Button
+      isHome={isHome}
+      hasNotifications={hasNotifications}
+      mentions={mentions}
+      className={selected ? 'active' : ''}
+
+    >
+      {isHome && <img src="https://svgshare.com/i/Pb9.svg" alt="Logo" />}
+    </Button>
+  )
+}
+
+export default ServerButton;
---
+++ src/components/ServerButton/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import { IButtonProps } from './';
+
+export const Button = styled.button<IButtonProps>`
+
+`;
